ea914d8a9d63123d72145a10b46d3132f1cdcd39
Meryem Ozge Uzun <76754183+mozgeuzun@users.noreply.github.com>
2022-08-08 12:17:15 +0300

    reactproject

---
+++ reactproject/App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View,Button, TextInput} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.appContainer}>     
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.textInput} placeholder='Your Course Goal'/>
+          <Button title="Add Goal"/>
+        </View>
+        <View>
+          <Text>List of goals...</Text>
+        </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    appContainer:{
+      padding: 50,
+    },
+    inputContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between'
+    },
+    textInput: {
+      borderWidth: 1,
+      borderColor: '#cccccc',
+      width: '80%',
+      marginRight: 8,
+      padding: 8
+    }
+});
